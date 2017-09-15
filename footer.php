30e68ca422ac58a52e1a0023bdba51098327027a
DSingh <human.divanshu@gmail.com>
2017-09-15 12:03:38 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id="footer">
+	<div class="container">
+		<div class="row" style="padding-top:40px;">
+		    <div class="col-md-12 text-center">
+				<p>copyright &copy; <a style="text-decoration:none;" href="#"><span style="font-size:18px;" class="logofont">my<span class="logoblue">campus</span>trips</span></a></p>
+		    </div>
+		</div>
+	</div>
+</div>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <?php require_once("common-head.php"); ?>
+    <title>Website Name</title>
+</head>
+<body>
+<?php require_once("nav.php"); ?>
+    <div class="container">
+    
+    </div>    
+<?php require_once("footer.php"); ?>
+    
+
+
+</body>
+</html>
